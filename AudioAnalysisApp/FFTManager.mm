9e7cb6b5e661ebee81dadc81e7134a651c7b3349
Mac Wibbels <wibbels.mac@gmail.com>
2013-07-08 00:12:06 -0400

    wtf is going on

--- AudioAnalysisApp/FFTManager.mm
+++ AudioAnalysisApp/FFTManager.mm
@@ -36,9 +36,9 @@ const int LOG_N = 10;
     
     // For polar coordinates
     float *mag = new float[originalSize / 2];
+    float *mag_Db = new float[originalSize / 2];
     float *phase = new float[originalSize / 2];
     
-    // Forward FFT:
     // Scramble-pack the real data into the complex buffer in the way
     // that it's required by the real-to-complex FFT function.
     vDSP_ctoz((DSPComplex *)x, 2, &tempSplitComplex, 1, originalSize / 2);
@@ -53,12 +53,19 @@ const int LOG_N = 10;
     
     // Convert from complex/rectangular (real & imaginary) coordinates
     // to polar (magnitude & phase) coordinates
+    float mic_scale = (originalSize) * 2;
     vDSP_zvabs(&tempSplitComplex, 1, mag, 1, originalSize / 2);
     vDSP_zvphas(&tempSplitComplex, 1, phase, 1, originalSize / 2);
+    vDSP_vdbcon(mag, 1, &mic_scale, mag_Db, 1, originalSize/2, 0);
     
     printf("\nMag / Phase:\n");
     for (int k = 0; k < originalSize / 2; k++) {
         printf("%3d\t%6.2f\t%6.2f\n", k, mag[k], phase[k]);
+    }
+    float freqStep =  22000/ (originalSize / 2);
+    printf("\nDecibels:\n");
+    for (int k = 0; k < originalSize / 2; k++) {
+        printf("%f\t%f\n", (k * freqStep), mag_Db[k]);
     }
     
     // Convert from polar coords back to rectangular coords
@@ -78,7 +85,7 @@ const int LOG_N = 10;
     vDSP_ztoc(&tempSplitComplex, 1, (DSPComplex *)y, 2, originalSize / 2);
     
     // Compensate for scaling for both FFTs
-    float scale = 0.5 / originalSize;
+    float scale = 1.0 / originalSize;
     vDSP_vsmul(y, 1, &scale, y, 1, originalSize);
     
     // For a sinusodial wave, input x and input y vectors will have identical values
